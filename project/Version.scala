a22b9856ec69932248e2f87ab3244f78035b3ee4
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-29 21:39:30 -0600

    [core] Initial squeleton
    Signed-off-by: Fabián Heredia Montiel <fabianhjr@protonmail.com>

---
+++ project/Version.scala
@@ -0,0 +1,15 @@
+object Version {
+  // Scala
+  lazy val scala = "3.2.2"
+
+  // Typelevel
+
+  lazy val cats               = "2.9.0"
+  lazy val catsEffect         = "3.4.8"
+  lazy val fs2                = "3.6.1"
+  lazy val http4s             = "0.23.18"
+  lazy val mouse              = "1.2.1"
+  lazy val scalacheck         = "1.17.0"
+  lazy val scalatest          = "3.2.15"
+  lazy val scalatestPlusCheck = scalatest + ".0"
+}
